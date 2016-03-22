cd8afd2ca3ba7ec385ad781fd1617932b69df9ac
Nick Cappadona <nickypasta@gmail.com>
2016-03-22 17:58:08 -0400

    Vendorize Bitters
    We'll be using it as a baseline and extending it. This seems to be the best
    approach in light of BEM and Bourbon v5.
    
    It will still allow us to track the Bitters version and easily update when
    necessary. It has the added benefit of enabling injection of our config in place
    of the default Bitters variables, which will not be used, by calling each Bitter
    component directly (see base/_base.scss).

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 
-var sassDir = 'stylesheets';
-var sassVenDir = path.join(sassDir, 'vendor');
+var sassDir = path.join('stylesheets', 'vendor');
 
 module.exports = {
   context: __dirname,
@@ -24,16 +23,16 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([
       // Bitters
-      { from: 'node_modules/Bitters/core', to: path.join(sassDir, 'base') },
+      { from: 'node_modules/Bitters/core', to: path.join(sassDir, 'bitters') },
       // Bourbon
-      { from: 'node_modules/bourbon/core', to: path.join(sassVenDir, 'bourbon') },
+      { from: 'node_modules/bourbon/core', to: path.join(sassDir, 'bourbon') },
       // Breakpoint
-      { from: 'node_modules/breakpoint-sass/stylesheets', to: path.join(sassVenDir, 'breakpoint') },
+      { from: 'node_modules/breakpoint-sass/stylesheets', to: path.join(sassDir, 'breakpoint') },
       // Normalize
-      { from: 'node_modules/normalize-scss/sass', to: path.join(sassVenDir, 'normalize') },
-      { from: 'node_modules/support-for/sass/_support-for.scss', to: path.join(sassVenDir, 'normalize') },
+      { from: 'node_modules/normalize-scss/sass', to: path.join(sassDir, 'normalize') },
+      { from: 'node_modules/support-for/sass/_support-for.scss', to: path.join(sassDir, 'normalize') },
       // Susy
-      { from: 'node_modules/susy/sass', to: path.join(sassVenDir, 'susy') }
+      { from: 'node_modules/susy/sass', to: path.join(sassDir, 'susy') }
     ], {
         ignore: []
     })
