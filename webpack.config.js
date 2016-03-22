e14bf2569925d39b92004818b44155d08c94d60e
Nick Cappadona <nickypasta@gmail.com>
2016-03-22 13:13:36 -0400

    Major overhaul to stylesheet architecture
    Includes several empty directories as placeholders to help better illustrate the
    overall direction we're headed. Also, the holdout directory is only temporary,
    holding the original stylesheets for reference as we build out the theme. Its
    days are numbered...
    
    Our own twist on The 7-1 Pattern (7 folders, 1 file)
    http://sass-guidelin.es/#the-7-1-pattern
    
    Most likely will become the 7-3 Pattern by moving variables out to the top level
    as _config.scss and also adding the aptly named _shame.scss for all necessary
    evils (hacks):
    http://csswizardry.com/2013/04/shame-css-full-net-interview

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 // Using npm + Webpack to manage vendor dependencies
-// -- for now Webpack is simply copying src files from modules to public/vendor
-// -- no compiling and public/vendor is not tracked thanks to .gitignore
-// -- provides some sanity for clean @import paths
-// -- while tracking versions & dependencies via package.json
+// -- for now Webpack is simply copying src files from modules to public dir
+// -- no compiling/preprocessing and copied files are not tracked thanks to .gitignore
+// -- provides some sanity for clean @import paths while tracking versions & dependencies via package.json
 
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
+
+var sassDir = 'stylesheets';
+var sassVenDir = path.join(sassDir, 'vendor');
 
 module.exports = {
   context: __dirname,
@@ -14,24 +16,24 @@ module.exports = {
 
   // Vendorize the mixins & libraries
   output: {
-    path: path.join(__dirname, 'public', 'vendor'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js', // Not currently used but required for Webpack to run
-    publicPath: '/public/vendor/'
+    publicPath: '/'
   },
 
   plugins: [
     new CopyWebpackPlugin([
       // Bitters
-      { from: 'node_modules/Bitters/core', to: 'base' },
+      { from: 'node_modules/Bitters/core', to: path.join(sassDir, 'base') },
       // Bourbon
-      { from: 'node_modules/bourbon/core', to: 'bourbon' },
+      { from: 'node_modules/bourbon/core', to: path.join(sassVenDir, 'bourbon') },
       // Breakpoint
-      { from: 'node_modules/breakpoint-sass/stylesheets', to: 'breakpoint' },
+      { from: 'node_modules/breakpoint-sass/stylesheets', to: path.join(sassVenDir, 'breakpoint') },
       // Normalize
-      { from: 'node_modules/normalize-scss/sass', to: 'normalize' },
-      { from: 'node_modules/support-for/sass/_support-for.scss', to: 'normalize/' },
+      { from: 'node_modules/normalize-scss/sass', to: path.join(sassVenDir, 'normalize') },
+      { from: 'node_modules/support-for/sass/_support-for.scss', to: path.join(sassVenDir, 'normalize') },
       // Susy
-      { from: 'node_modules/susy/sass', to: 'susy' }
+      { from: 'node_modules/susy/sass', to: path.join(sassVenDir, 'susy') }
     ], {
         ignore: []
     })
