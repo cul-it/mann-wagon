e9f698628265c298e5ca6c7dc06cbf0f260f88f6
Tahir Poduska <tap87@cornell.edu>
2016-04-26 14:50:16 -0400

    Using components for read more feature for displaying event description

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -343,3 +343,56 @@ var events = new Vue({
     }
 
 })
+
+
+// Define component for read more description feature
+var Description = Vue.extend({
+  // props, passed via custom element %description
+  props: ['descriptionText', 'wordLimit'],
+  data: function () {
+    return {
+      // limit description to number of words
+       descriptionLimit: this.wordLimit,
+      // read more/less links
+       readMoreLink: true,
+       readLessLink: false,
+      //  Array for words in description
+       inputWords: this.descriptionText.split(/\s+/)
+      }
+  },
+  ready: function() {
+  },
+  filters: {
+    limitDescriptionFilter: function(description, wordLimit) {
+      if (this.inputWords.length > wordLimit) {
+          description = this.inputWords.slice(0, wordLimit).join(' ');
+      }
+      // Hide read more link if description <= description word limit
+      else if (this.inputWords.length <= wordLimit) {
+        this.readMoreLink = false;
+      };
+      return(description);
+    }
+
+  },
+  methods: {
+    showMoreDescription(){
+      // set description limit to the number of words
+      this.descriptionLimit = this.inputWords.length;
+      // hide/show read more/less link
+      this.readMoreLink = false;
+      this.readLessLink = true;
+    },
+    showLessDescription(){
+      // Reset description limit
+      this.descriptionLimit = this.wordLimit;
+      // hide/show read more/less link
+      this.readMoreLink = true;
+      this.readLessLink = false;
+    }
+  },
+  template: '<p class="description" > ((descriptionText | limitDescriptionFilter descriptionLimit)) <a href="javascript:;" v-on:click="showMoreDescription()" v-show="readMoreLink"> ....Read More</a><a href="javascript:;" v-on:click="showLessDescription()" v-show="readLessLink"> Read Less </a></p>'
+})
+
+// register
+Vue.component('description', Description)
