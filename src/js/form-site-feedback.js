37f1498bcf6ee7f76c44d908e8b6ea345bf7e583
Nick Cappadona <nickypasta@gmail.com>
2016-07-22 07:48:17 -0400

    Request referer via Locomotive's global drop
    Liquid FTW. locomotivecms/steam#73

--- src/js/form-site-feedback.js
+++ src/js/form-site-feedback.js
@@ -5,24 +5,6 @@ import 'semantic-ui-css/components/message.min.css';
 import 'semantic-ui-css/components/transition.min.css';
 import 'semantic-ui-css/components/transition.js';
 
-var siteFeedback = {
-  onLoad: function() {
-    this.urlField = $('.mann-form__url');
-    this.nabReferer();
-  },
-
-  nabReferer: function() {
-    // If url field is empty, inject page referer
-    if (siteFeedback.urlField.val() == '') {
-      $('.mann-form__url').val(document.referrer);
-    }
-  }
-}
-
-$(document).ready(function() {
-  siteFeedback.onLoad();
-});
-
 // Would like to use semantic-ui dropdown component but mobile usability is awful
 // -- limits the number of displayed options to 3 and scrolling is not obvious
 // -- this is configurable via a LESS variable
