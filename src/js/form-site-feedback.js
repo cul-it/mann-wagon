601a4638ec9729925ae96e1831382b329ddf35f8
Nick Cappadona <nickypasta@gmail.com>
2016-07-22 12:38:26 -0400

    Comment out dropdown component for semantic-ui
    Not currently using it due to poor mobile UX, but leave it here for future
    reference in case we want to revisit.

--- src/js/form-site-feedback.js
+++ src/js/form-site-feedback.js
@@ -1,5 +1,5 @@
 import 'semantic-ui-css/components/dropdown.min.css';
-import 'semantic-ui-css/components/dropdown.js';
+// import 'semantic-ui-css/components/dropdown.min.js';
 import 'semantic-ui-css/components/form.min.css';
 import 'semantic-ui-css/components/message.min.css';
 import 'semantic-ui-css/components/transition.min.css';
