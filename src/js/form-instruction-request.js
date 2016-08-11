ef2591e9b680ed3824701dea8880c11545ac6bf8
Nick Cappadona <cappadona@users.noreply.github.com>
2016-08-11 14:48:03 -0400

    Address semantic-ui duplication introduced by calendar component (#436)
    * Use popup component from semantic-ui-css package
    
    As we do for all other components provided as part of semantic-ui core.
    
    * Drop npm dependency on semantic-ui calendar fork
    
    Introduced in 8513bdc / 5fa6ce6 (duped due to inadvertent rebasing).
    
    Resorting to downloading the minified assets for calendar component directly
    from GitHub fork for now as recommended by contributor:
    https://github.com/Semantic-Org/Semantic-UI/pull/3256#issuecomment-224113834
    
    Storing in `src/js/vendor` which has been added as a resolvable path in webpack
    config.
    
    Downloaded latest as of today (mdehoog/Semantic-UI@2c10c1f)
    https://github.com/mdehoog/Semantic-UI/tree/calendar-dist
    
    Calendars on instruction request form are fully functional, but the Booked
    reservation form needs a minor tweak...up next.
    
    * Initialize sui calendar component after modal
    
    Calendar component failed to initialize properly when it was called via onLoad
    after ripping out semantic-ui duplication.
    
    This quick-n-dirty fix addresses the issue but this is still ripe for additional
    refactoring. It looks like Booked is about get the boot, so I'm going no
    further at this point.

--- src/js/form-instruction-request.js
+++ src/js/form-instruction-request.js
@@ -1,16 +1,18 @@
+// SUI calendar component via mdehoog fork until PR is merged
+// -- https://github.com/mdehoog/Semantic-UI/tree/calendar-dist
+// -- https://github.com/Semantic-Org/Semantic-UI/pull/3256#issuecomment-224113834
+import 'semantic-ui-calendar/calendar.min.js';
+import 'semantic-ui-calendar/calendar.min.css';
 
 import 'semantic-ui-css/components/dropdown.min.css';
 import 'semantic-ui-css/components/dropdown.min.js';
 import 'semantic-ui-css/components/form.min.css';
 import 'semantic-ui-css/components/form.min.js';
 import 'semantic-ui-css/components/message.min.css';
-import 'semantic-ui-css/components/transition.min.css';
-import 'semantic-ui-css/components/transition.min.js';
 import 'semantic-ui-css/components/popup.min.css';
 import 'semantic-ui-css/components/popup.min.js';
-// this is using the calendar plugin to semantic-ui that tahir pulled in
-import 'semantic-ui/dist/components/calendar.min.js'
-import 'semantic-ui/dist/components/calendar.min.css'
+import 'semantic-ui-css/components/transition.min.css';
+import 'semantic-ui-css/components/transition.min.js';
 
 $('.date-selector').calendar({
   type: 'date'
@@ -20,7 +22,7 @@ $('.date-selector').calendar({
 // i see nick's comment on the feedback form that he doesn't like this for mobile
 // probably needs to be reworked, but i do like the multiple select that comes with semantic-ui
 // another caveat - because we're not using the "semantic-ui way" of using font-awesome icons:
-// the multiple select doesn't show the "x" to remove an item 
+// the multiple select doesn't show the "x" to remove an item
 // the calendar doesn't show the < > (previous/next) icons on month view
 $('.ui.dropdown')
   .dropdown()
@@ -62,7 +64,7 @@ $('.ui.form')
             prompt  : 'Please select at least one type of instruction'
         }]
       },
-      // would like to use moment and a custom inline function for date validation 
+      // would like to use moment and a custom inline function for date validation
       // don't see a way to do this using semantic ui form validation
       // date_first: {
       //   identifier  : 'content[date_first]',
