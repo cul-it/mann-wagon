7578e1f4a488759ed76f854eabf8296aff93ecb9
Nick Cappadona <nickypasta@gmail.com>
2016-08-12 12:34:28 -0400

    Drop sui embed component & use iframe directly
    Default template for embed component disables scrolling within iframe:
    https://github.com/Semantic-Org/Semantic-UI/blob/2.2.2/dist/components/embed.js#L669
    
    But we scrolling because the content of the iframe changes (expands) as
    user interacts with LibCal form.
    
    The main downside to this approach is that we need to set an explicit iframe
    height in pixels. Currently using 625px, but might end up reducing it if I can
    remove some elements (header, breadcrumb, footer) from iframe content. Nice
    thing is that because the modal is responsive this adapts nicely at all
    breakpoints.
    
    It does appear that you can provide a custom template for embed component, but
    not in the mood to mess with it right now. Can revisit later if necessary:
    http://semantic-ui.com/modules/embed.html#/settings

--- src/js/modal-libcal.js
+++ src/js/modal-libcal.js
@@ -1,5 +1,3 @@
-import 'semantic-ui-css/components/embed.min.css';
-import 'semantic-ui-css/components/embed.min.js';
 import 'semantic-ui-css/components/modal.min.js';
 import 'semantic-ui-css/components/modal.min.css';
 
@@ -10,14 +8,7 @@ var modalLibcal = {
 
   bindEventListeners: function() {
     $('.js-modal-libcal').on('click', function() {
-      $('.ui.embed')
-        .embed()
-      ;
-
       $('.js-sui-modal')
-        .modal({
-          observeChanges: true
-        })
         .modal('show')
       ;
     });
