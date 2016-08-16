514f1d35e364d108a82ddc8f0ed94d35bbdd37aa
Nick Cappadona <cappadona@users.noreply.github.com>
2016-08-16 10:32:13 -0400

    Adios booked (#459)
    * Strip all (most) references of Booked
    
    Leaving events (Vue.js) related integrations for Tahir to address.
    
    * Use snippet for reservable space button
    
    DRY it up.
    
    * Trigger LibCal widget vs. full via content data
    
    Introduces new field for LibCal group public page URL (aka friendly URL) and
    abstracts out the specific implementation used to demo with Mann 102 in #456.
    
    * No need to wrap reserve button within an anchor
    
    This was only causing the browser to append a pound sign to the URL which makes
    the history messy. Yes, another approach could have been to use
    event.preventDefault(), but this is simpler and cleaner.
    
    P.S. The title attribute originally on the anchor, was moved to the button.
    
    * Clarify differences btw reservation system fields
    
    Make it a point to clearly indicate that two of them apply to LibCal only.

--- src/js/modal-libcal.js
+++ src/js/modal-libcal.js
@@ -1,5 +1,9 @@
+import 'semantic-ui-css/components/dimmer.min.js';
+import 'semantic-ui-css/components/dimmer.min.css';
 import 'semantic-ui-css/components/modal.min.js';
 import 'semantic-ui-css/components/modal.min.css';
+import 'semantic-ui-css/components/transition.min.js';
+import 'semantic-ui-css/components/transition.min.css';
 
 var modalLibcal = {
   onLoad: function() {
