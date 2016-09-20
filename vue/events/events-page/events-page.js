0e0e53fc10ba5bf93cf0facf0ec57d72f3df36e6
Tahir Poduska <tap87@cornell.edu>
2016-09-20 16:25:21 -0400

    1. Use vue-router for the app, to prevent page reload on url changes when opening and closing modal (#554)
    2. Modal from homepage stays on  homepage
    3. Set the date of the modal to the event clicked

--- vue/events/events-page/events-page.js
+++ vue/events/events-page/events-page.js
@@ -1,12 +1,26 @@
 var Vue = require('vue')
 Vue.use(require('vue-resource'))
+var VueRouter = require('vue-router')
+Vue.use(VueRouter)
+// create router
+const router = new VueRouter({
+  history: true,
+  saveScrollPosition: true
+})
+
 // Change Vue delimiters, to work with liquid
 Vue.config.delimiters = ['((', '))']
 Vue.config.unsafeDelimiters = ['(((', ')))']
 
 var Events = require('./components/Events.vue')
 
-new Vue({
-  el: 'body',
-  components: { Events }
+var App = Vue.extend({})
+
+router.map({
+  '/news-events/events/upcoming': {
+    name: 'events',
+    component: Events
+  }
 })
+
+router.start(App, 'body')
--- vue/events/homepage-events/homepage-events.js
+++ vue/events/homepage-events/homepage-events.js
@@ -1,20 +1,26 @@
 var Vue = require('vue')
-Vue.use(require('vue-resource'));
+Vue.use(require('vue-resource'))
+var VueRouter = require('vue-router')
+Vue.use(VueRouter)
+// create router
+const router = new VueRouter({
+  history: true,
+  saveScrollPosition: true
+})
 
 // Change Vue delimiters, to work with liquid
-Vue.config.delimiters = ['((', '))'];
+Vue.config.delimiters = ['((', '))']
 Vue.config.unsafeDelimiters = ['(((', ')))']
 
-// require a *.vue component
 var HomePageEvents = require('./components/HomepageEvents.vue')
 
+var App = Vue.extend({})
 
-// mount a root Vue instance
-new Vue({
-  el: 'body',
-  components: {
-    // include the required component
-    // in the options
-    events: HomePageEvents
+router.map({
+  '/': {
+    name: 'homepage_events',
+    component: HomePageEvents
   }
 })
+
+router.start(App, 'body')
