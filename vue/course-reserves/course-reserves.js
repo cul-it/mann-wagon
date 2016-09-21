da9966b2b9ad68ea51d66f0c435ad3a4497ff18d
Nick Cappadona <nickypasta@gmail.com>
2016-09-21 10:27:09 -0400

    Build course list in Liquid = server-side caching
    This eliminates the CourseReserves Vue component, since the root vm can now pass
    off directly to the ReserveItem component.
    
    I didn't see a better way around the long load times for the course list from
    libservices. Using Locomotive's consume tag, we can cache the response (only
    applies when the site is hosted via engine). Currently caching for 1 hr.
    
    Looking into vue-resource, it does appear to be possible to cache the response
    via interceptors, but this would still be client-side which means any visitor
    would experience this initial long load for course listings.
    https://github.com/vuejs/vue-resource/issues/252#issuecomment-243611698
    https://github.com/vuejs/vue-resource/blob/master/docs/http.md#interceptors

--- vue/course-reserves/course-reserves.js
+++ vue/course-reserves/course-reserves.js
@@ -7,9 +7,51 @@ Vue.use(VueResource)
 Vue.config.delimiters = ['((', '))']
 Vue.config.unsafeDelimiters = ['(((', ')))']
 
-import CourseReserves from './components/course-reserves'
+// Vue components
+import ReserveItem from './components/reserve-item'
+
+// Styles
+import 'semantic-ui-css/components/loader.min.css'
+import 'components/course-reserves'
 
 new Vue({
-  el: 'body',
-  components: { CourseReserves }
+  el: '.course-reserves',
+
+  components: {
+    ReserveItem
+  },
+
+  data () {
+    return {
+      items: [],
+      itemsCourse: '',
+      selectedCourse: ''
+    }
+  },
+
+  // Watch course list and retrieve items when a course is selected
+  watch: {
+    selectedCourse: 'getReserveItems'
+  },
+
+  methods: {
+    getReserveItems () {
+      if ( this.selectedCourse ) {
+        var reservesApiBaseUrl = 'http://mannservices.mannlib.cornell.edu/LibServices/showCourseReserveItemInfo.do?output=json&courseid='
+        var reservesApiUrl = reservesApiBaseUrl + this.selectedCourse
+
+        this.toggleLoader()
+
+        this.$http.get(reservesApiUrl).then(function (response) {
+          this.$set('itemsCourse', response.data.course)
+          this.$set('items', response.data.reserveItemList)
+
+          this.toggleLoader()
+        })
+      }
+    },
+    toggleLoader () {
+      $('.course-reserves__loader').toggleClass('active')
+    }
+  }
 })
