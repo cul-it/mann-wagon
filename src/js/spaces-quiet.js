f41b16ac78944c26273b076febe8665da59dad45
Nick Cappadona <cappadona@users.noreply.github.com>
2016-09-27 15:08:14 -0400

    Today at Mann tweaks (#583)
    * New label for Find all Spaces button #440
    
    * Drop consultation section from available now #501
    
    Rework Today at Mann (aka available now) layout to minimize vertical space by
    splitting up study spaces into two groupings/columns. Thanks to Susy for making
    this painless. Logic for splitting up study spaces is far from ideal...it
    expects 8 study spaces and splits them in half, but this can be improved to be
    more flexible if we adjust the number of study spaces down the road.
    
    * Next available popup for study spaces via sui
    
    Been wanting to add this enhancement for a while...
    
    * Drop spaces-quiet.js
    
    No longer necessary. Follow lead from next available status in previous commit
    651fc6f and use sui's CSS only version of the popup component (tooltip).
    
    http://semantic-ui.com/modules/popup.html#tooltip
    
    * Top left position for available now sui popup
    
    Leads to more reliable behavior of actually displaying the tooltip on-screen
    when at smaller breakpoints. Kind of a moot point since the popup component is
    non-functional for touch devices, but just in case that ever changes down the
    road ;)

--- src/js/spaces-quiet.js
+++
@@ -1,8 +0,0 @@
-import 'semantic-ui-css/components/popup.min.js';
-import 'semantic-ui-css/components/popup.min.css';
-import 'semantic-ui-css/components/transition.min.js';
-import 'semantic-ui-css/components/transition.min.css';
-
-$('.js-sui-popup')
-  .popup()
-;
