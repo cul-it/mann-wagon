278babbdbe8da928c39473033c8ac2fc596fd815
Tahir Poduska <tap87@cornell.edu>
2016-10-19 09:40:37 -0400

    Temporarily disabling r25 calls, and hardcoding Workshop/Class  for beta launch (#729)

--- vue/events/events-page/directives/datepicker-directive.js
+++ vue/events/events-page/directives/datepicker-directive.js
@@ -10,7 +10,7 @@ Vue.directive('datepicker', {
         vm.localistReservations = vm.libcalReservations = vm.r25Reservations = []
         vm.getCornellEvents('date', date)
         vm.getLibCalEvents('date', date)
-        vm.getR25Events('date', date)
+        // vm.getR25Events('date', date)
         vm.showNoEventsMessage = true
         vm.allEvents = []
         vm.dateSelected = date
