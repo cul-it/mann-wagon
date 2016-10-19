e9b7a33a29b780691c18a49a0104164b72ab5bf3
Nick Cappadona <nickypasta@gmail.com>
2016-10-19 02:27:24 -0400

    Accessory nav for mobile breakpoints #359
    AKA hamburger AKA Hamburglar
    
    Includes My Account looking the part as an imposter member ;)

--- src/js/nav-search.js
+++ src/js/nav-search.js
@@ -4,6 +4,15 @@ var navSearch = {
   },
 
   bindEventListeners: function() {
+    $('.accessory-nav__button').on('click', function() {
+      // Toggle reveal of accessory nav plus my account
+      $('.accessory-nav__list').toggleClass('accessory-nav__list--hamburglarin');
+      $('.my-account').toggleClass('my-account--hamburglarin');
+
+      // Toggle button state to trigger differences in rendering/style
+      $(this).toggleClass('accessory-nav__button--active');
+    });
+
     $('.search__button').on('click', function() {
       // Focus on search input
       $('.search__term').focus();
