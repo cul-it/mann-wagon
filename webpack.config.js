cf6d6532d06e8ee89d423abb575e7245a4d38e3c
Nick Cappadona <nickypasta@gmail.com>
2016-10-25 19:57:58 -0400

    Drop beta banner #775

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
   },
   entry: {
       allExperts: './src/js/all-experts.js',
-      betaBanner: './src/js/beta-banner.js',
       consultationRequest: './src/js/form-consultation-request.js',
       courseReserves: './vue/course-reserves/course-reserves.js',
       eventsPage: './vue/events/events-page/events-page.js',
