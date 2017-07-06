e9161a981ebe25aaf8986954b13d4ef4ebfd7285
Nick Cappadona <nickypasta@gmail.com>
2017-07-06 02:49:27 -0400

    Move BrowserSyncPlugin to Webpack dev config

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -2,7 +2,6 @@ import Config from 'webpack-config'
 import path from 'path'
 
 // Plugins
-import BrowserSyncPlugin from 'browser-sync-webpack-plugin'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import WebpackShellPlugin from 'webpack-shell-plugin'
 
@@ -109,30 +108,6 @@ export default new Config().merge({
     fs: 'empty'
   },
   plugins: [
-    new BrowserSyncPlugin(
-      // BrowserSync options
-      {
-        // Browse app at http://localhost:3000 during development
-        host: 'localhost',
-        port: 3000,
-        // Proxy `wagon serve` through Browsersync
-        // -- see `start script` in package.json
-        proxy: 'localhost:3333',
-        // Files to watch
-        files: [
-          'public/stylesheets/**/*',
-          'public/javascripts/**/*',
-          'app/views/**/*'
-        ]
-      },
-      // Plugin options
-      {
-        // Don't force Browsersync to reload
-        // -- let the watched files trigger it
-        // -- allows for CSS injection without full page reload
-        reload: false
-      }
-    ),
     // Extract compiled CSS from bundle
     new ExtractTextPlugin('../stylesheets/[name].css'),
     // Remove extraneous bundle leftover after extracting CSS
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,37 @@
 import Config from 'webpack-config'
 
+// Plugins
+import BrowserSyncPlugin from 'browser-sync-webpack-plugin'
+
 export default new Config().extend('webpack.base.config.js').merge({
   debug: true,
   output: {
     pathinfo: true
-  }
+  },
+  plugins: [
+    new BrowserSyncPlugin(
+      // BrowserSync options
+      {
+        // Browse app at http://localhost:3000 during development
+        host: 'localhost',
+        port: 3000,
+        // Proxy `wagon serve` through Browsersync
+        // -- see `start script` in package.json
+        proxy: 'localhost:3333',
+        // Files to watch
+        files: [
+          'public/stylesheets/**/*',
+          'public/javascripts/**/*',
+          'app/views/**/*'
+        ]
+      },
+      // Plugin options
+      {
+        // Don't force Browsersync to reload
+        // -- let the watched files trigger it
+        // -- allows for CSS injection without full page reload
+        reload: false
+      }
+    )
+  ]
 })
