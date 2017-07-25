00655a1c2660f5745f782dee6bb48aa219a2bef6
Nick Cappadona <nickypasta@gmail.com>
2017-07-25 16:43:44 -0400

    Single bundle for homepage carousels
    Results in calling slick-carousel a single time to build both carousels,
    which allows for Slick's internal incrementing to properly assign unique
    IDs and in turn addresses final error for 1.3.1 Info and Relationships:
    
    * Use unique identification for elements

--- src/js/carousels.js
+++ src/js/carousels.js
@@ -1,8 +1,37 @@
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import 'components/meet-experts'
+import 'components/carousels'
 
 import 'slick-carousel'
+
+$('.spotlights__images').slick({
+  centerMode: true,
+  dots: true,
+  dotsClass: 'slick-dots carousel__dots',
+  mobileFirst: true,
+  nextArrow: $('.js-spotlights-next'),
+  prevArrow: $('.js-spotlights-prev'),
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        centerPadding: '50px'
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        centerPadding: '75px'
+      }
+    },
+    {
+      breakpoint: 896,
+      settings: {
+        centerPadding: '100px'
+      }
+    }
+  ]
+})
 
 $('.meet-experts__librarians').slick({
   dots: true,
