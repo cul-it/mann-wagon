59cd6c1de8748600e1c770a3b7a30b0ba64d2481
Nick Cappadona <cappadona@users.noreply.github.com>
2018-11-20 08:55:44 -0500

    Instruct request updates (#982)
    * Overhaul course instruction request form
    
    Laundry list of changes [1] as per Ashley's request. Really pushing the
    limits here of Locomotive's form submission capabilities.
    
    [1] cul-it/mann-sitefeedback#295
    
    * More breathing room for other input fields
    
    * Tweak required fields
    
    Add
    
    * Department
    * Cornell affiliation
    * Instruction needs/learning outcomes
    
    Drop
    
    * Phone
    
    > As per Ashley's 10/24/18 feedback
    
    * Drop additional course info field
    
    Hasta la vista...you win some, you lose some ;)
    
    * Call out library guides for online materials
    
    * Datetime field: 48hrs in advance, M-F 8am-8pm
    
    * Reorder and relabel additional info field
    
    Asking more explicitly for info on related assignment

--- src/js/form-instruction-request.js
+++ src/js/form-instruction-request.js
@@ -14,11 +14,60 @@ import 'semantic-ui-css/components/popup.min.js'
 import 'semantic-ui-css/components/transition.min.css'
 import 'semantic-ui-css/components/transition.min.js'
 
-$('.date-selector').calendar({
-  type: 'date'
-})
+import { escape } from 'lodash'
+import moment from 'moment'
 
-$('.ui.checkbox').checkbox()
+$('.date-selector').calendar({
+  type: 'datetime',
+  // Require at least 48 hrs advanced notice and up to 3 months out
+  minDate: moment().add(2, 'day').toDate(),
+  maxDate: moment().add(3, 'month').toDate(),
+
+  // Is the given date disabled?
+  isDisabled: function (date, mode) {
+    var day = moment(date).day()
+    var hour = moment(date).hour()
+    var minute = moment(date).minute()
+    // Sunday = 0, Saturday = 6
+    var weekend = [0, 6]
+    // Standard hours are 8am - 8pm, Mon-Fri
+    var open = 8
+    var close = 20
+
+    switch (mode) {
+      case 'day':
+        // Closed on the weekend
+        if (weekend.indexOf(day) !== -1) {
+          return true
+        }
+        break
+      case 'hour':
+        if (hour < open || hour >= close) {
+          return true
+        }
+
+        // Catch day exceptions if browsing through days in hour mode
+        if (weekend.indexOf(day) !== -1) {
+          return true
+        }
+        break
+      case 'minute':
+        // Don't allow registrations within 15min of closing
+        if (hour === (close - 1)) {
+          if (minute > 45) {
+            return true
+          }
+        }
+
+        // Catch hour & day exceptions if browsing through days in minute mode
+        if (hour >= close || (weekend.indexOf(day) !== -1)) {
+          return true
+        }
+        break
+    }
+    return false
+  }
+})
 
 // FEW ITEMS TO NOTE
 // i see nick's comment on the feedback form that he doesn't like this for mobile
@@ -45,48 +94,89 @@ $('.ui.form')
           prompt: 'Please enter a valid e-mail'
         }]
       },
-      phone: {
-        identifier: 'content[phone]',
-        rules: [{
-          type: 'regExp[^(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?(?:\\(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\\s*\\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\\s*(?:[.-]\\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\\s*(?:[.-]\\s*)?([0-9]{4})(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+))?$]',
-          prompt: 'Please enter a valid phone number'
-        }]
-      },
-      // for some reason the "on blur" event doesn't seem to trigger this validation rule
-      // only working on form submit
-      // -- nac26 2016-09-02: this is a side-effect of using SUI's dropdown component
-      instruction_types: {
-        identifier: 'content[instruction_types]', // Target data-validate attrib; SUI choked on trailing `[]` in field name
+      department: {
+        identifier: 'content[department]',
         rules: [{
           type: 'empty',
+          prompt: 'Please enter your department name'
+        }]
+      },
+      affiliation: {
+        identifier: 'content[affiliation]',
+        rules: [{
+          type: 'empty',
+          prompt: 'Please select your affiliation'
+        }]
+      },
+      course_name: {
+        identifier: 'content[course_name]',
+        rules: [{
+          type: 'empty',
+          prompt: 'Please enter the course name'
+        }]
+      },
+      course_number: {
+        identifier: 'content[course_number]',
+        rules: [{
+          type: 'empty',
+          prompt: 'Please enter the course number'
+        }]
+      },
+      instruction_types: {
+        identifier: 'content[instruction_types][]',
+        rules: [{
+          type: 'checked',
           prompt: 'Please select at least one type of instruction'
         }]
+      },
+      location: {
+        identifier: 'content[location]',
+        rules: [{
+          type: 'checked',
+          prompt: 'Please select your preferred location'
+        }]
+      },
+      needs: {
+        identifier: 'needs',
+        rules: [{
+          type: 'empty',
+          prompt: 'Please provide details on your instruction needs and/or learning outcomes'
+        }]
+      },
+      assignment: {
+        identifier: 'assignment',
+        rules: [{
+          type: 'empty',
+          prompt: 'Please indicate whether there will be a related assignment'
+        }]
       }
-      // would like to use moment and a custom inline function for date validation
-      // don't see a way to do this using semantic ui form validation
-      // date_first: {
-      //   identifier  : 'content[date_first]',
-      //   optional  : true,
-      //   rules: [{
-      //       type    : 'regExp[]'
-      //       prompt  : 'Please enter a valid date'
-      //   }]
-      // },
-      // date_second: {
-      //   identifier  : 'content[date_second]',
-      //   optional  : true,
-      //   rules: [{
-      //       type    : 'regExp[]'
-      //       prompt  : 'Please enter a valid date'
-      //   }]
-      // },
-      // date_third: {
-      //   identifier  : 'content[date_third]',
-      //   optional  : true,
-      //   rules: [{
-      //       type    : 'regExp[]'
-      //       prompt  : 'Please enter a valid date'
-      //   }]
-      // }
     }
   })
+
+var otherValues = {
+  onLoad: function () {
+    this.bindEventListeners()
+    this.prepOthers()
+  },
+
+  bindEventListeners: function () {
+    $('.js-mannform-other__value').blur(function () {
+      otherValues.appendUserContent($(this))
+    })
+  },
+
+  appendUserContent: function (trigger) {
+    const userValue = escape(trigger.val()) || 'unspecified'
+    const otherOption = trigger.siblings('.js-mannform-other')
+    const label = otherOption.attr('data-prefix')
+    otherOption.val(label + ': ' + userValue)
+  },
+
+  prepOthers: function () {
+    $('.js-mannform-other__value').get().forEach(v => otherValues.appendUserContent($(v)))
+  }
+}
+
+$(document).ready(function () {
+  otherValues.onLoad()
+})
