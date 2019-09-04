cul-it/mann-wagon

commit cc7f36df0df63b702a3db0c6ea33e1ccbfd68a76

Check for null values in libcal data from mannservices. If null value, try again. (#774)

vue/events/events-page/components/Events.vue
@@ -744,70 +744,84 @@ export default {
       // Event properties
       _.forEach(data, function (value, index) {
         var events = {}
-        if (value.description.match('Will this be advertised through Cornell Events\\?: (.*)')[1] == 'Yes') {
-          // Do nothing
-        } else {
-          // If same event based on title and time comparison
-          // Check twice for lobby
-          if (libcalEvents.length && libcalEvents[counter - 1].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 1].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
-            libcalEvents[counter - 1].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
-          } else if (libcalEvents.length > 1 && libcalEvents[counter - 2].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 2].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
-            libcalEvents[counter - 2].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
+        // Workaround for situations were incomplete data is returned from MannServices
+        if(value.description.match('Will this be advertised through Cornell Events\\?: (.*)') === null || value.description.match('Event Name: (.*)') === null || value.description.match('Event Description: (.*)') === null) {
+          console.log(value);
+          vueInstance.$set('libcalReservations', [])
+          if (vueInstance.dateSelected) {
+            vueInstance.getLibCalEvents('date', vueInstance.dateSelected)
           } else {
-            events['event_id'] = value.eventId
-            events['event_title'] = value.description.match('Event Name: (.*)')[1]
-            events['event_description'] = value.description.match('Event Description: (.*)')[1]
-            events['event_start_time'] = moment(new Date(value.formattedStartDateTime)).format()
-            events['event_start'] = moment(new Date(value.formattedStartDateTime)).format('YYYY-MM-DD')
-            events['event_end_time'] = moment(new Date(value.formattedEndDateTime)).format()
-            events['event_room_name'] = value.location.trim().replace(',', '')
-            _.forEach(vueInstance.curatedEventLocations, function(curatedEventLocation, index) {
-              if (curatedEventLocation[0] === events['event_room_name'] || _.includes(curatedEventLocation[1], events['event_room_name'])) {
-                  events['event_room_name'] = curatedEventLocation[0]
-                  events['event_room_smartmap_url'] = curatedEventLocation[2]
-              }
-            })
-            events['event_type'] = [value.description.match('Event type: (.*)')[1].trim().replace(',', '')]
-            _.forEach(vueInstance.curatedEventTypes, function(curatedEventType, index) {
-              if (curatedEventType[0] === value.description.match('Event type: (.*)')[1].trim().replace(',', '') || _.includes(curatedEventType[1], value.description.match('Event type: (.*)')[1].trim().replace(',', ''))) {
-                events['event_type'] = [curatedEventType[0]]
-              }
-            })
-
-            // Events array from LibCal
-            libcalEvents.push(events)
-            // Increment event counter
-
-          // Room filter list array
-          if (roomNames.indexOf(value.location.trim().replace(',', '')) === -1) {
-            roomNames.push(value.location.trim().replace(',', ''))
+            vueInstance.getLibCalEvents('default')
           }
-          // Event type filter list array
-          if (eventTypes.indexOf(value.description.match('Event type: (.*)')[1].trim().replace(',', '')) === -1) {
-            eventTypes.push(value.description.match('Event type: (.*)')[1].trim().replace(',', ''))
-          }
-            counter++
+        }
+        if (vueInstance.libcalReservations.length) {
+          if (value.description.match('Will this be advertised through Cornell Events\\?: (.*)')[1] == 'Yes') {
+            // Already in Localist, Do nothing
+          } else {
+            // If same event based on title and time comparison
+            // Check twice for lobby
+            if (libcalEvents.length && libcalEvents[counter - 1].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 1].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
+              libcalEvents[counter - 1].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
+            } else if (libcalEvents.length > 1 && libcalEvents[counter - 2].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 2].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
+              libcalEvents[counter - 2].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
+            } else {
+              events['event_id'] = value.eventId
+              events['event_title'] = value.description.match('Event Name: (.*)')[1]
+              events['event_description'] = value.description.match('Event Description: (.*)')[1]
+              events['event_start_time'] = moment(new Date(value.formattedStartDateTime)).format()
+              events['event_start'] = moment(new Date(value.formattedStartDateTime)).format('YYYY-MM-DD')
+              events['event_end_time'] = moment(new Date(value.formattedEndDateTime)).format()
+              events['event_room_name'] = value.location.trim().replace(',', '')
+              _.forEach(vueInstance.curatedEventLocations, function(curatedEventLocation, index) {
+                if (curatedEventLocation[0] === events['event_room_name'] || _.includes(curatedEventLocation[1], events['event_room_name'])) {
+                    events['event_room_name'] = curatedEventLocation[0]
+                    events['event_room_smartmap_url'] = curatedEventLocation[2]
+                }
+              })
+              events['event_type'] = [value.description.match('Event type: (.*)')[1].trim().replace(',', '')]
+              _.forEach(vueInstance.curatedEventTypes, function(curatedEventType, index) {
+                if (curatedEventType[0] === value.description.match('Event type: (.*)')[1].trim().replace(',', '') || _.includes(curatedEventType[1], value.description.match('Event type: (.*)')[1].trim().replace(',', ''))) {
+                  events['event_type'] = [curatedEventType[0]]
+                }
+              })
+
+              // Events array from LibCal
+              libcalEvents.push(events)
+              // Increment event counter
+
+            // Room filter list array
+            if (roomNames.indexOf(value.location.trim().replace(',', '')) === -1) {
+              roomNames.push(value.location.trim().replace(',', ''))
+            }
+            // Event type filter list array
+            if (eventTypes.indexOf(value.description.match('Event type: (.*)')[1].trim().replace(',', '')) === -1) {
+              eventTypes.push(value.description.match('Event type: (.*)')[1].trim().replace(',', ''))
+            }
+              counter++
+            }
           }
         }
       })
-      _.forEach(eventTypes, function (type, index, eventTypes) {
-        _.forEach(vueInstance.curatedEventTypes, function(curatedEventType) {
-          if (curatedEventType[0] === type || _.includes(curatedEventType[1], type)) {
-              eventTypes[index] = curatedEventType[0]
-          }
+      if (vueInstance.libcalReservations.length) {
+        _.forEach(eventTypes, function (type, index, eventTypes) {
+          _.forEach(vueInstance.curatedEventTypes, function(curatedEventType) {
+            if (curatedEventType[0] === type || _.includes(curatedEventType[1], type)) {
+                eventTypes[index] = curatedEventType[0]
+            }
+          })
         })
-      })
-      _.forEach(roomNames, function (room, index, roomNames) {
-        _.forEach(vueInstance.curatedEventLocations, function(curatedEventLocation) {
-          if (curatedEventLocation[0] === room || _.includes(curatedEventLocation[1], room)) {
-              roomNames[index] = curatedEventLocation[0]
-          }
+        _.forEach(roomNames, function (room, index, roomNames) {
+          _.forEach(vueInstance.curatedEventLocations, function(curatedEventLocation) {
+            if (curatedEventLocation[0] === room || _.includes(curatedEventLocation[1], room)) {
+                roomNames[index] = curatedEventLocation[0]
+            }
+          })
         })
-      })
-      // Set array values to be used later to merge
-      this.$set('libcalEvents', libcalEvents)
-      this.$set('libcalEventTypes', eventTypes)
-      this.$set('libcalRoomNames', roomNames)
+        // Set array values to be used later to merge
+        this.$set('libcalEvents', libcalEvents)
+        this.$set('libcalEventTypes', eventTypes)
+        this.$set('libcalRoomNames', roomNames)
+      }
     },
     // Custom data model from r25 events
     r25EventsArray (data) {

vue/events/homepage-events/components/HomepageEvents.vue
@@ -502,44 +502,56 @@ export default {
       // Event properties
       _.forEach(data, function (value, index) {
         var events = {}
-        if (value.description.match('Will this be advertised through Cornell Events\\?: (.*)')[1] == 'Yes') {
-          // Do nothing
-        } else {
-          // If same event based on title and time comparison
-          // Check twice for lobby
-          if (libcalEvents.length && libcalEvents[counter - 1].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 1].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
-            libcalEvents[counter - 1].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
-          } else if (libcalEvents.length > 1 && libcalEvents[counter - 2].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 2].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
-            libcalEvents[counter - 2].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
+        // Workaround for situations were incomplete data is returned from MannServices
+        if(value.description.match('Will this be advertised through Cornell Events\\?: (.*)') === null || value.description.match('Event Name: (.*)') === null || value.description.match('Event Description: (.*)') === null) {
+          console.log(value);
+          vueInstance.$set('libcalReservations', [])
+          vueInstance.getLibCalEvents('default')
+        }
+        if (vueInstance.libcalReservations.length) {
+          if (value.description.match('Will this be advertised through Cornell Events\\?: (.*)')[1] == 'Yes') {
+            // Already in Localist, Do nothing
           } else {
-            events['event_id'] = value.eventId
-            events['event_title'] = value.description.match('Event Name: (.*)')[1]
-            events['event_description'] = value.description.match('Event Description: (.*)')[1]
-            events['event_start_time'] = moment(new Date(value.formattedStartDateTime)).format()
-            events['event_start'] = moment(new Date(value.formattedStartDateTime)).format('YYYY-MM-DD')
-            events['event_end_time'] = moment(new Date(value.formattedEndDateTime)).format()
-            events['event_room_name'] = value.location.trim().replace(',', '')
-            _.forEach(vueInstance.curatedEventLocations, function(curatedEventLocation, index) {
-              if (curatedEventLocation[0] === events['event_room_name'] || _.includes(curatedEventLocation[1], events['event_room_name'])) {
-                  events['event_room_name'] = curatedEventLocation[0]
-                  events['event_room_smartmap_url'] = curatedEventLocation[2]
-              }
-            })
-            events['event_type'] = [value.description.match('Event type: (.*)')[1].trim().replace(',', '')]
-            _.forEach(vueInstance.curatedEventTypes, function(curatedEventType, index) {
-              if (curatedEventType[0] === value.description.match('Event type: (.*)')[1].trim().replace(',', '') || _.includes(curatedEventType[1], value.description.match('Event type: (.*)')[1].trim().replace(',', ''))) {
-                events['event_type'] = [curatedEventType[0]]
-              }
-            })
-            // Events array from LibCal
-            libcalEvents.push(events)
-            counter++
+            // If same event based on title and time comparison
+            // Check twice for lobby
+            if (libcalEvents.length && libcalEvents[counter - 1].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 1].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
+              libcalEvents[counter - 1].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
+            } else if (libcalEvents.length > 1 && libcalEvents[counter - 2].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 2].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
+              libcalEvents[counter - 2].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
+            } else {
+              events['event_id'] = value.eventId
+              events['event_title'] = value.description.match('Event Name: (.*)')[1]
+              events['event_description'] = value.description.match('Event Description: (.*)')[1]
+              events['event_start_time'] = moment(new Date(value.formattedStartDateTime)).format()
+              events['event_start'] = moment(new Date(value.formattedStartDateTime)).format('YYYY-MM-DD')
+              events['event_end_time'] = moment(new Date(value.formattedEndDateTime)).format()
+              events['event_room_name'] = value.location.trim().replace(',', '')
+              _.forEach(vueInstance.curatedEventLocations, function(curatedEventLocation, index) {
+                if (curatedEventLocation[0] === events['event_room_name'] || _.includes(curatedEventLocation[1], events['event_room_name'])) {
+                    events['event_room_name'] = curatedEventLocation[0]
+                    events['event_room_smartmap_url'] = curatedEventLocation[2]
+                }
+              })
+              events['event_type'] = [value.description.match('Event type: (.*)')[1].trim().replace(',', '')]
+              _.forEach(vueInstance.curatedEventTypes, function(curatedEventType, index) {
+                if (curatedEventType[0] === value.description.match('Event type: (.*)')[1].trim().replace(',', '') || _.includes(curatedEventType[1], value.description.match('Event type: (.*)')[1].trim().replace(',', ''))) {
+                  events['event_type'] = [curatedEventType[0]]
+                }
+              })
+
+              // Events array from LibCal
+              libcalEvents.push(events)
+              // Increment event counter
+
+              counter++
+            }
           }
         }
       })
-
-      // Set array values to be used later to merge
-      this.$set('libcalEvents', libcalEvents)
+      if (vueInstance.libcalReservations.length) {
+        // Set array values to be used later to merge
+        this.$set('libcalEvents', libcalEvents)
+      }
     },
     // Custom data model from r25 events
     r25EventsArray (data) {

commit d02296122ecda5c403962272cf3746268e196c98

Events calendar gracefully handles missing data (#1009)
* Events calendar fails gracefully w/ missing data

Traced the underlying issue down to a previous workaround introduced
in #774 as part of the original site launch back in Oct 2016 which
attempted to catch null/empty values for several fields and reset the
events array. This came to a boil this week when the events calendar was
stuck in an endless loop, both on the home page and full events calendar
page, due to missing/empty values for advertising with CU Events
calendar, event name and event description

This commit introduces a failsafe to ensure the events calendar degrades
gracefully. The patron submitted values embedded in the description field
of the LibServices relay feed are parsed and missing values are handled
explicitly. To be clear, all of these fields are required on the public
form, but staff with LibCal accounts often bypass the form and add a
reservation directly with minimal info (date and time) :(

We first encountered this missing values bug in Jan 2018 during the
LibCal E&S migration with event_type field. I should have listened to
myself at that point and implemented this failsafe for the rest of the
.match() occurrences at that time #938. It's actually kind of incredible
that we haven't run into this endless loop scenario prior to this point.

* Reorganize code for sanity

Rename variable introduced in previous work to have sensible defaults
for event_type (#938) for consistency with the rest of the failsafe
handling/parsing from this PR.

* Apply the same logic & handling to homepage

Unfortunately this is almost a verbatim dupe between the main Vue app
for events calendar page and the embedded Vue app on the homepage.

So. Not. DRY.

vue/events/events-page/components/Events.vue
@@ -750,30 +750,36 @@ export default {
       // Event properties
       _.forEach(data, function (value, index) {
         var events = {}
-        // Workaround for situations were incomplete data is returned from MannServices
-        if(value.description.match('Will this be advertised through Cornell Events\\?: (.*)') === null || value.description.match('Event Name: (.*)') === null || value.description.match('Event Description: (.*)') === null) {
-          console.log(value);
-          vueInstance.$set('libcalReservations', [])
-          if (vueInstance.dateSelected) {
-            vueInstance.getLibCalEvents('date', vueInstance.dateSelected)
-          } else {
-            vueInstance.getLibCalEvents('default')
-          }
-        }
-        if (vueInstance.libcalReservations.length) {
-          if (value.description.match('Will this be advertised through Cornell Events\\?: (.*)')[1] == 'Yes') {
+
+        // Parse patron submitted values from description
+        // -- handle missing values – all of these are required on the public form, but staff often bypass :(
+        // -- first encountered missing values bug in 2018-01-19 LibCal E&S migration with event_type
+        // -- should have listened to myself and implemented this failsafe for the rest of the .match() occurrences at that time
+        // -- bit by this in a big way in 2019-09-03 when the events calendar was stuck in an endless loop
+        // ---- due to missing/empty values for advertising with CU Events calendar, event name and event description
+        // -- https://j11y.io/javascript/match-trick
+        const eventAdvertise = (value.description.match('Will this be advertised through Cornell Events\\?: (.*)') || [, false])[1]
+        const submittedName = value.description.match('Event Name: (.*)')[1]
+        const eventTitle = submittedName === '' ? 'No title provided' : submittedName
+        const submittedDescription = value.description.match('Event Description: (.*)')[1]
+        const eventDescription = submittedDescription === '' ? 'No description provided' : submittedDescription
+        const submittedEventType = (value.description.match('Event Type: (.*)') || [, ''])[1].trim().replace(',', '')
+        const eventType = submittedEventType === '' ? 'MISSING' : submittedEventType
+
+        if (vueInstance.libcalReservations.length) {          
+          if (eventAdvertise === 'Yes') {
             // Already in Localist, Do nothing
           } else {
             // If same event based on title and time comparison
             // Check twice for lobby
-            if (libcalEvents.length && libcalEvents[counter - 1].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 1].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
+            if (libcalEvents.length && libcalEvents[counter - 1].event_title === eventTitle && libcalEvents[counter - 1].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
               libcalEvents[counter - 1].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
-            } else if (libcalEvents.length > 1 && libcalEvents[counter - 2].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 2].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
+            } else if (libcalEvents.length > 1 && libcalEvents[counter - 2].event_title === eventTitle && libcalEvents[counter - 2].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
               libcalEvents[counter - 2].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
             } else {
               events['event_id'] = value.eventId
-              events['event_title'] = value.description.match('Event Name: (.*)')[1]
-              events['event_description'] = value.description.match('Event Description: (.*)')[1]
+              events['event_title'] = eventTitle
+              events['event_description'] = eventDescription
               events['event_start_time'] = moment(new Date(value.formattedStartDateTime)).format()
               events['event_start'] = moment(new Date(value.formattedStartDateTime)).format('YYYY-MM-DD')
               events['event_end_time'] = moment(new Date(value.formattedEndDateTime)).format()
@@ -785,16 +791,10 @@ export default {
                 }
               })
 
-              // Default value of 'MISSING' for event_type
-              // -- if this custom question is not included in description
-              // -- encountered after 2018-01-19 LibCal E&S migration
-              // -- probably wise to have this failsafe in place for all occurrences of .match()
-              // -- https://j11y.io/javascript/match-trick
-              const eventTypeSelected = (value.description.match(/Event Type: (.*)/i) || [,'MISSING'])[1].trim().replace(',', '')
-              events['event_type'] = [eventTypeSelected]
+              events['event_type'] = [eventType]
 
               _.forEach(vueInstance.curatedEventTypes, function(curatedEventType, index) {
-                if (curatedEventType[0] === eventTypeSelected || _.includes(curatedEventType[1], eventTypeSelected)) {
+                if (curatedEventType[0] === eventType || _.includes(curatedEventType[1], eventType)) {
                   events['event_type'] = [curatedEventType[0]]
                 }
               })
@@ -809,8 +809,8 @@ export default {
             }
 
             // Event type filter list array
-            if (eventTypes.indexOf(eventTypeSelected) === -1) {
-              eventTypes.push(eventTypeSelected)
+            if (eventTypes.indexOf(eventType) === -1) {
+              eventTypes.push(eventType)
             }
               counter++
             }

vue/events/homepage-events/components/HomepageEvents.vue
@@ -505,26 +505,36 @@ export default {
       // Event properties
       _.forEach(data, function (value, index) {
         var events = {}
-        // Workaround for situations were incomplete data is returned from MannServices
-        if(value.description.match('Will this be advertised through Cornell Events\\?: (.*)') === null || value.description.match('Event Name: (.*)') === null || value.description.match('Event Description: (.*)') === null) {
-          console.log(value);
-          vueInstance.$set('libcalReservations', [])
-          vueInstance.getLibCalEvents('default')
-        }
+        
+        // Parse patron submitted values from description
+        // -- handle missing values – all of these are required on the public form, but staff often bypass :(
+        // -- first encountered missing values bug in 2018-01-19 LibCal E&S migration with event_type
+        // -- should have listened to myself and implemented this failsafe for the rest of the .match() occurrences at that time
+        // -- bit by this in a big way in 2019-09-03 when the events calendar was stuck in an endless loop
+        // ---- due to missing/empty values for advertising with CU Events calendar, event name and event description
+        // -- https://j11y.io/javascript/match-trick
+        const eventAdvertise = (value.description.match('Will this be advertised through Cornell Events\\?: (.*)') || [, false])[1]
+        const submittedName = value.description.match('Event Name: (.*)')[1]
+        const eventTitle = submittedName === '' ? 'No title provided' : submittedName
+        const submittedDescription = value.description.match('Event Description: (.*)')[1]
+        const eventDescription = submittedDescription === '' ? 'No description provided' : submittedDescription
+        const submittedEventType = (value.description.match('Event Type: (.*)') || [, ''])[1].trim().replace(',', '')
+        const eventType = submittedEventType === '' ? 'MISSING' : submittedEventType
+
         if (vueInstance.libcalReservations.length) {
-          if (value.description.match('Will this be advertised through Cornell Events\\?: (.*)')[1] == 'Yes') {
+          if (eventAdvertise === 'Yes') {
             // Already in Localist, Do nothing
           } else {
             // If same event based on title and time comparison
             // Check twice for lobby
-            if (libcalEvents.length && libcalEvents[counter - 1].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 1].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
+            if (libcalEvents.length && libcalEvents[counter - 1].event_title === eventTitle && libcalEvents[counter - 1].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
               libcalEvents[counter - 1].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
-            } else if (libcalEvents.length > 1 && libcalEvents[counter - 2].event_title === value.description.match('Event Name: (.*)')[1] && libcalEvents[counter - 2].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
+            } else if (libcalEvents.length > 1 && libcalEvents[counter - 2].event_title === eventTitle && libcalEvents[counter - 2].event_end_time === moment(new Date(value.formattedStartDateTime)).format()) {
               libcalEvents[counter - 2].event_end_time = moment(new Date(value.formattedEndDateTime)).format()
             } else {
               events['event_id'] = value.eventId
-              events['event_title'] = value.description.match('Event Name: (.*)')[1]
-              events['event_description'] = value.description.match('Event Description: (.*)')[1]
+              events['event_title'] = eventTitle
+              events['event_description'] = submittedDescription
               events['event_start_time'] = moment(new Date(value.formattedStartDateTime)).format()
               events['event_start'] = moment(new Date(value.formattedStartDateTime)).format('YYYY-MM-DD')
               events['event_end_time'] = moment(new Date(value.formattedEndDateTime)).format()
@@ -536,16 +546,10 @@ export default {
                 }
               })
 
-              // Default value of 'MISSING' for event_type
-              // -- if this custom question is not included in description
-              // -- encountered after 2018-01-19 LibCal E&S migration
-              // -- probably wise to have this failsafe in place for all occurrences of .match()
-              // -- https://j11y.io/javascript/match-trick
-              const eventTypeSelected = (value.description.match(/Event Type: (.*)/i) || [,'MISSING'])[1].trim().replace(',', '')
-              events['event_type'] = [eventTypeSelected]
+              events['event_type'] = [eventType]
 
               _.forEach(vueInstance.curatedEventTypes, function(curatedEventType, index) {
-                if (curatedEventType[0] === eventTypeSelected || _.includes(curatedEventType[1], eventTypeSelected)) {
+                if (curatedEventType[0] === eventType || _.includes(curatedEventType[1], eventType)) {
                   events['event_type'] = [curatedEventType[0]]
                 }
               })